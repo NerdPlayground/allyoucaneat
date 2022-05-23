1fb935318379120353e867d209562dbe103f0598
Paqimm <mcgeewilliam815@gmail.com>
2022-05-23 18:59:12 +0300

    chore (App) : add orders model and register to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,30 @@
+from orders.models import Order
 from django.contrib import admin
+from products.models import Content
+from django.utils.html import format_html_join
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display= [
+        "id","product","order_contents",
+        "customer","paid","created_on"
+    ]
+    search_fields= ["customer"]
+    list_filter= ["paid"]
+
+    def order_contents(self,obj):
+        contents= Content.objects.filter(orders=obj.id)
+        return format_html_join(
+            '\n',"<li style='list-style-type:none;'>{}</li>",
+            (content for content in contents)
+        )
+
+    def has_add_permission(self,request):
+        return False
+
+    def has_change_permission(self,request):
+        return False
+
+    def has_delete_permission(self,request):
+        return False
+
+admin.site.register(Order,OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    id= models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    product= models.ForeignKey(
+        "products.Product",
+        related_name="orders",
+        on_delete= models.DO_NOTHING
+    )
+    contents= models.ManyToManyField(
+        "products.Content",
+        related_name="orders",
+    )
+    customer= models.ForeignKey(
+        "custom_user.User",
+        related_name="orders",
+        on_delete= models.DO_NOTHING
+    )
+    paid= models.BooleanField(default=False)
+    created_on= models.DateTimeField(auto_now_add=True)
