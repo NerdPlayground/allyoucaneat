91ef8cfcadb34449f8c71b792f3c9caf89465f6e
Paqimm <mcgeewilliam815@gmail.com>
2022-07-27 13:15:43 +0300

    refractor (Database) : use PostgreSQL

--- allyoucaneat/settings.py
+++ allyoucaneat/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'allyoucaneat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dballyoucaneat',
+        'USER': 'postgres',
+        'PASSWORD': 'PostgreSQL23!@#',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
